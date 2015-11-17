84bdd7bf0825918e276f7f70c0ae2a10373a02bf
200NyuRa <karyakina.ann@gmail.com>
2015-11-17 16:51:02 +0400

    вебинар 1 задание 2 доработка1

--- js/check.js
+++ js/check.js
@@ -1,47 +1,48 @@
 'use strict';
 
 function getMessage(a, b) {
-// переменные для разных типов данных
-  var DataTypeBoolean = "boolean";
-  var DataTypeNamber = "number";
-  var DataTypeObject = "object";
+  // переменные для разных типов данных
+  var DataTypeBoolean = 'boolean';
+  var DataTypeNamber = 'number';
+  var DataTypeObject = 'object';
 
   //условие - первый аргумент имеет тип boolean
-  if (typeof a == DataTypeBoolean) {
-    if (a == true){
-      alert( 'Переданное GIF-изображение анимировано и содержит '+ b +' кадров');
-    }
-    else {
-      alert( "Переданное GIF-изображение не анимировано");
-    }
+  if (a == true){
+    var message = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+  }
+  else if (a == false){
+    var message = 'Переданное GIF-изображение не анимировано';
   }
 
   //условие - первый аргумент имеет числовой тип
   else if (typeof a == DataTypeNamber){
-    alert( "Переданное SVG-изображение содержит "+ a +" объектов и "+(b * 4)+ " аттрибутов");
+    var message = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов';
   }
 
   //условие - первый аргумент имеет тип объект
-  else if (typeof a == DataTypeObject && a.length>0 && typeof b !== DataTypeObject){
+  else if (typeof a == DataTypeObject && a.length > 0 && typeof b !== DataTypeObject){
     var sum = 0;
-    for (var i = 0; i<a.length; i++){
+    for (var i = 0; i < a.length; i++){
       sum += a[i];
     }
-   alert( "Количество красных точек во всех строчках изображения " + sum);
+    var message = 'Количество красных точек во всех строчках изображения ' + sum;
   }
 
   //условие - оба аргумент имеют тип объект
-  else if (typeof a == DataTypeObject && a.length>0 && typeof b == DataTypeObject && b.length>0){
+  else if (typeof a == DataTypeObject && a.length > 0 && typeof b == DataTypeObject && b.length > 0){
     var square = 0;
     //сравниваем длину аргументов - массивов
-    var minLengthArray = Math.min(a.length,b.length)
+    var minLengthArray = Math.min(a.length, b.length);
 
-      //колличество перемножаемых элементов каждого массива равно количеству элеменнов в массиве меньшей длины
-      for (var i = 0; i<minLengthArray; i++){
-        square += a[i]*b[i];
-      }
+    //колличество перемножаемых элементов каждого массива равно количеству элеменнов в массиве меньшей длины
+    for (var i = 0; i < minLengthArray; i++){
+      square += a[i] * b[i];
+    }
 
-    alert( "Общая площадь артефактов сжатия: "+square+" пикселей");
+    var message = 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
   }
+
+  return message;
 }
+
 
